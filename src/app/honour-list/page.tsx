707b46eb99c2cff3f3c17bc10147df85775fe77a
laithm <laith.masri@gmail.com>
2025-10-20 22:10:14 +0100

    added the last pages

---
+++ src/app/honour-list/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+import Header from '../components/Header';
+import HonourList from '../components/HonourList';
+
+export default function HonourListPage() {
+  return (
+    <>
+      <Header />
+      <main>
+        <HonourList />
+      </main>
+    </>
+  );
+}
+
---
+++ src/app/project-of-the-term/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+import Header from '../components/Header';
+import ProjectOfTerm from '../components/ProjectOfTerm';
+
+export default function ProjectOfTermPage() {
+  return (
+    <>
+      <Header />
+      <main>
+        <ProjectOfTerm />
+      </main>
+    </>
+  );
+}
+
