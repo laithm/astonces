c595849792a3c3f698aca24bb0a37aea27dd37ef
laithm <laith.masri@gmail.com>
2025-10-20 22:10:36 +0100

    added career support

---
+++ src/app/career-support/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+import Header from '../components/Header';
+import CareerSupportForm from '../components/CareerSupportForm';
+
+export default function CareerSupportPage() {
+  return (
+    <>
+      <Header />
+      <main>
+        <CareerSupportForm />
+      </main>
+    </>
+  );
+}
+
