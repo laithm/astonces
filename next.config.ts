afa077c77e1ad52d30cd4bf7442087bb382a06ef
laithm <laith.masri@gmail.com>
2025-10-21 12:47:40 +0100

    rules fix up 6

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
+    typescript: { ignoreBuildErrors: true },
   },
 };
 export default nextConfig;
